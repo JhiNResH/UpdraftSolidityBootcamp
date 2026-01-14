65447f2f6e5e7ec1d108eb36c9048d17de657027
JhiNResH <dontforgetbananatiff@gmail.com>
2026-01-13 20:31:34 -0800

    interactions

--- Foundry/foundry-nft-f23/script/Interactions.s.sol
+++ Foundry/foundry-nft-f23/script/Interactions.s.sol
@@ -5,16 +5,21 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol"
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script {
+    string public constant gf1 = "ipfs://bafybeihs6njua4n6ikobowx7p7z6ahy7spy2z7refzp2vv7vcq4wyfkk5m/?filename=gf1.json";
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
         "BasicNft", 
         block.chainid
         );
-        mintNftOnContract(MostRecentlyDeployed)
+        mintNftOnContract(mostRecentlyDeployed);
     }
 
-    function mintNftOnContract()
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(gf1);
+        vm.stopBroadcast();
+    }
 }
